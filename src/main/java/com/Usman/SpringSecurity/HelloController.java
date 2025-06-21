dca0cf5af8fcb75678eabd2609aee331f6dc6aa8
Usman Eletu <usmaneletu2@gmail.com>
2025-06-22 00:27:59 +0100

    first commit

---
+++ src/main/java/com/Usman/SpringSecurity/HelloController.java
@@ -0,0 +1,13 @@
+package com.Usman.SpringSecurity;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String greet(){
+        return "hello welcome";
+    }
+}
