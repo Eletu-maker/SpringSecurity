ac0cd40dd24b547f81086d26620a2551a20b29d8
Usman Eletu <usmaneletu2@gmail.com>
2025-06-22 02:33:31 +0100

    commit

--- src/main/java/com/Usman/SpringSecurity/HelloController.java
+++ src/main/java/com/Usman/SpringSecurity/HelloController.java
@@ -1,5 +1,8 @@
 package com.Usman.SpringSecurity;
 
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 
     @GetMapping("/")
-    public String greet(){
-        return "hello welcome";
+    public String greet(HttpServletRequest request){
+        return "hello welcome" + request.getSession().getId();
     }
 }
---
+++ src/main/java/com/Usman/SpringSecurity/studentController.java
@@ -0,0 +1,38 @@
+package com.Usman.SpringSecurity;
+
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+@RestController
+public class studentController {
+
+    private List<Student> students  = new ArrayList<>(List.of(
+            new Student(1,"usman", 57),
+            new Student(2,"eletu", 67),
+            new Student(3,"magic", 58)
+    ));
+
+    @GetMapping("/students")
+    public List<Student> getStudent(){
+        return students;
+    }
+
+    @PostMapping("/student")
+    public Student addStudent(@RequestBody Student student){
+        students.add(student);
+        return student;
+    }
+
+    @GetMapping("/csrf_token")
+    public CsrfToken csrfToken (HttpServletRequest request){
+        return (CsrfToken) request.getAttribute("_csrf");
+    }
+
+}
